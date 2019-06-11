1fca5cd7d5e55901a5e2a6e388661614bb4fbbaa
berkayyildiz97 <berkay97yildiz@gmail.com>
2019-06-11 14:59:03 +0300

    Added Toasts

--- app/src/main/java/com/example/gastrogang/LoginActivity.java
+++ app/src/main/java/com/example/gastrogang/LoginActivity.java
@@ -3,24 +3,74 @@ package com.example.gastrogang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private Toolbar actionbarLogin;
+    private EditText txtUsername, txtPassword;
+    private Button btnLogin;
 
     public void init() {
         actionbarLogin = findViewById(R.id.actionbarLogin);
         setSupportActionBar(actionbarLogin);
         getSupportActionBar().setTitle("GİRİŞ YAP");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtUsername = (EditText) findViewById(R.id.txtUsernameLogin);
+        txtPassword = (EditText) findViewById(R.id.txtPasswordLogin);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+
     }
 
+
+    private void loginUser() {
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Kullanıcı Adı giriniz!", Toast.LENGTH_LONG).show();
+
+        }
+        else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Şifre giriniz!", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            //TODO: HTTP REQUEST
+
+            /*
+            if (isSuccessful) {
+                Toast.makeText(LoginActivity.this, "Başarılı bir şekilde giriş yapıldı!", Toast.LENGTH_LONG).show();
+                Intent viewIntent = new Intent(LoginActivity.this, ViewActivity.class);
+                startActivity(viewIntent);
+                finish();
+            }
+            else {
+                Toast.makeText(LoginActivity.this, "Giriş yaparken bir hata oluştu!", Toast.LENGTH_LONG).show();
+            }
+            */
+
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/gastrogang/RegisterActivity.java
+++ app/src/main/java/com/example/gastrogang/RegisterActivity.java
@@ -1,18 +1,64 @@
 package com.example.gastrogang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Toolbar actionbarRegister;
+    private EditText txtUsername, txtPassword;
+    private Button btnRegister;
 
     public void init() {
         actionbarRegister = (Toolbar) findViewById(R.id.actionbarRegister);
         setSupportActionBar(actionbarRegister);
         getSupportActionBar().setTitle("KAYIT OL");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtUsername = (EditText) findViewById(R.id.txtUsernameRegister);
+        txtPassword = (EditText) findViewById(R.id.txtPasswordRegister);
+        btnRegister = (Button) findViewById(R.id.buttonRegister);
+    }
+
+    private void createNewAccount() {
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Kullanıcı Adı boş olamaz!", Toast.LENGTH_LONG).show();
+
+        }
+        else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Şifre boş olamaz!", Toast.LENGTH_LONG).show();
+        }
+        else if (password.length() < 6) {
+            Toast.makeText(this, "Şifre en az 6 karakterli olmalı!", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            //TODO: HTTP REQUEST
+
+            /*
+            if (isSuccessful) {
+                Toast.makeText(RegisterActivity.this, "Hesap başarılı bir şekilde oluşturuldu!", Toast.LENGTH_LONG).show();
+                Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+            else {
+                Toast.makeText(RegisterActivity.this, "Kayıt olurken bir hata oluştu!", Toast.LENGTH_LONG).show();
+            }
+            */
+
+            
+        }
     }
 
     @Override
@@ -20,5 +66,12 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         init();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewAccount();
+            }
+        });
     }
 }
