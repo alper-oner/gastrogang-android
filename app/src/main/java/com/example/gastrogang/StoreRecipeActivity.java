0dd6b0e54ffac053626fb0c5b07fd4185eefa272
Alper Oner <alperoner0710@gmail.com>
2019-06-12 20:35:54 +0300

    Added Post method in StoreRecipeActivity. Added Volley.

--- app/src/main/java/com/example/gastrogang/StoreRecipeActivity.java
+++ app/src/main/java/com/example/gastrogang/StoreRecipeActivity.java
@@ -4,7 +4,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,12 +12,23 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class StoreRecipeActivity extends AppCompatActivity {
     ArrayList recipeStepList = new ArrayList<String>();
     String recipeName = new String();
     String recipeDetails = new String();
+    String url = "https://webhook.site/3e631a91-708d-43bc-8e48-b449514272c9"; // TODO add true url
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +64,7 @@ public class StoreRecipeActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
                                            int pos, long id) {
-                Toast.makeText(StoreRecipeActivity.this, "Item deleted" , Toast.LENGTH_SHORT).show();
+                Toast.makeText(StoreRecipeActivity.this, "Item deleted", Toast.LENGTH_SHORT).show();
                 recipeStepList.remove(pos);
                 arrayAdapter.notifyDataSetChanged();
                 return true;
@@ -68,7 +78,7 @@ public class StoreRecipeActivity extends AppCompatActivity {
 
                 recipeName = recipeNameText.getText().toString();
                 recipeDetails = recipeDetailsText.getText().toString();
-                if (recipeName.length()<=0 || recipeDetails.length() <=0 || recipeStepList.size()<=0) {
+                if (recipeName.length() <= 0 || recipeDetails.length() <= 0 || recipeStepList.size() <= 0) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(StoreRecipeActivity.this);
                     builder.setCancelable(true);
                     builder.setTitle("Error");
@@ -80,8 +90,41 @@ public class StoreRecipeActivity extends AppCompatActivity {
                         }
                     });
                     builder.show();
+                } else {
+                    JSONObject obj = new JSONObject();
+                    try {
+                        obj.put("name", recipeName);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        obj.put("steps", recipeStepList);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        obj.put("details", recipeDetails);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                    JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, url, obj,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Toast.makeText(StoreRecipeActivity.this, "Successful: " + response.toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(StoreRecipeActivity.this, "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                    queue.add(jsObjRequest);
                 }
-                Log.e("MyApp",recipeName + " " + recipeDetails);
             }
         });
 
