023c6ce6e9951ba11f3be483aa8b0b0e08b17470
berkayyildiz97 <berkay97yildiz@gmail.com>
2019-06-15 14:33:12 +0300

    Added GET Request for View. Added ingredients and edit button to design.

--- app/src/main/java/com/example/gastrogang/RecipeActivity.java
+++ app/src/main/java/com/example/gastrogang/RecipeActivity.java
@@ -8,31 +8,50 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private String recipeId = "";
+    private String recipeName = "";
+    private String recipeDetails = "";
+    private ArrayList<String> recipeIngredientsList = null;
+    private ArrayList<String> recipeStepsList = null;
+    private String ACCESS_TOKEN = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
-        TextView recipeName = findViewById(R.id.txtrcpRecipeName);
-
-        String[] recipeStepList = {"Slice", "Blend", "Age", "Cook"};
+        TextView txtviewRecipeName = findViewById(R.id.txtrcpRecipeName);
+        TextView txtviewRecipeDetails = findViewById(R.id.txtrcpRecipeDetails);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String value = extras.getString("recipeName");
-            recipeName.setText(value);
-            //TODO: SET recipeStepList and RecipeDetails
+            ACCESS_TOKEN = extras.getString("token");
+            recipeName = extras.getString("name");
+            recipeDetails = extras.getString("details");
+            recipeIngredientsList = extras.getStringArrayList("ingredients");
+            recipeStepsList = extras.getStringArrayList("steps");
         }
+        else {
+            Toast.makeText(RecipeActivity.this, "Unexpected Error" , Toast.LENGTH_LONG).show();
+        }
+        txtviewRecipeName.setText(recipeName);
+        txtviewRecipeDetails.setText(recipeDetails);
 
-        ListView lv = findViewById(R.id.rcpSteplist);
+        ListView lvIngredients = findViewById(R.id.rcpSteplist);
+        ArrayAdapter<String> ingredientsAdapter = new ArrayAdapter<String>
+                (getApplicationContext(), android.R.layout.simple_list_item_1, recipeIngredientsList);
+        lvIngredients.setAdapter(ingredientsAdapter);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (getApplicationContext(), android.R.layout.simple_list_item_1, recipeStepList);
-
-        lv.setAdapter(arrayAdapter);
+        ListView lvSteps = findViewById(R.id.rcpIngredientList);
+        ArrayAdapter<String> stepsAdapter = new ArrayAdapter<String>
+                (getApplicationContext(), android.R.layout.simple_list_item_1, recipeStepsList);
+        lvSteps.setAdapter(stepsAdapter);
 
         Button deleteRecipe = findViewById(R.id.btnDeleteRecipe);
 
@@ -42,7 +61,9 @@ public class RecipeActivity extends AppCompatActivity {
                 //TODO: HTTP REQUEST FOR DELETE
 
                 Intent intent = new Intent(RecipeActivity.this, ViewActivity.class);
+                intent.putExtra("token", ACCESS_TOKEN);
                 startActivity(intent);
+                finish();
             }
         });
     }
